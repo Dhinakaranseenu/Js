a8ae6003fd332742ef52cc7fe3d5b25134f1d087
Dhinakaranseenu <109011799+Dhinakaranseenu@users.noreply.github.com>
2022-12-18 11:58:59 +0530

    Add files via upload

---
+++ First.js
@@ -0,0 +1,21 @@
+/*document.write("hello");
+console.log("hi");
+alert("Are you ok?");
+confirm("choose me!");
+prompt("enter a value");*/
+
+var num = 5;
+var b = "naga";
+var c = null;
+var d = undefined;
+var e = true;
+var f = " ";
+var g;
+var h = 10;
+
+/*document.write(a+"*"+b+"*"+c);
+console.log(g);
+alert(b+h+a);*/
+
+document.write(num+"<br>"+c);
+
